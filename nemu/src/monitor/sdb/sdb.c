92fd6d54bb1d14feaa558cb1d4e6155a265cd966
tracer-ics2023 <tracer@njuics.org>
2024-03-26 20:45:02 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:45:02 up  1:33,  8 users,  load average: 1.95, 1.47, 1.18

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -52,14 +52,14 @@ static int cmd_si(char *args) {
   return 0;
 }
 
-// static int cmd_info(char *args) {
-//   if (strcmp(args, "r") == 0) {
-//     isa_reg_display(args);
-//   } else if (strcmp(args, "w") == 0) {
-
-//   }
-//   return 0;
-// }
+static int cmd_info(char *args) {
+  if (strcmp(args, "r") == 0) {
+    isa_reg_display(args);
+  } else if (strcmp(args, "w") == 0) {
+    
+  }
+  return 0;
+}
 
 static int cmd_q(char *args) {
   set_nemu_state(4, 0, 0);
@@ -76,7 +76,8 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  { "si", "Step through N commands", cmd_si}
+  { "si", "Step through N commands", cmd_si},
+  { "info", "Display regs or watchpoints", cmd_info}
   /* TODO: Add more commands */
 
 };
