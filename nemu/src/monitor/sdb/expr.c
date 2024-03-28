9e784229b9138915226fdc4deddc21256ec4a61d
tracer-ics2023 <tracer@njuics.org>
2024-03-28 11:13:33 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:13:33 up  1:36,  3 users,  load average: 0.51, 0.31, 0.53

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -41,7 +41,7 @@ static struct rule {
 
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
-  {"^==$", TK_EQ},        // equal
+  {"={2}", TK_EQ},        // equal
 
   {"\\-", '-'}, 
   {"\\*", '*'}, 
