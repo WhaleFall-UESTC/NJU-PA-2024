375e8d8ab5f775f813427d2e5b8f83294ce39471
tracer-ics2023 <tracer@njuics.org>
2024-04-09 10:50:00 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:50:00 up  1:45,  2 users,  load average: 0.53, 1.12, 1.11

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -68,7 +68,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   p += space_len;
 
 #ifndef CONFIG_ISA_loongarch32r
-  void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+  //void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
 #else
@@ -124,7 +124,7 @@ void cpu_exec(uint64_t n) {
 
     case NEMU_ABORT: {
       word_t top = cpu.pc + IRBUFSIZE;
-      word_t bottom = cpu.pc - IRBUFSIZE;
+      word_t bottom = (cpu.pc - IRBUFSIZE > 0x80000000) ? cpu.pc - IRBUFSIZE : 0x80000000;
       char inst_s[32];
       word_t inst_b;
       for (word_t pc = bottom; pc <= top; pc += 4) {
