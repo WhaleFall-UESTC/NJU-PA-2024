16e5f659d44f81025e337ea2cc7c49f1e1a7cddf
tracer-ics2023 <tracer@njuics.org>
2024-04-09 21:03:12 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:03:12 up  1:57,  5 users,  load average: 3.38, 1.60, 0.98

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -107,7 +107,6 @@ static int parse_args(int argc, char *argv[]) {
 
 void init_monitor(int argc, char *argv[]) {
   /* Perform some global initialization. */
-  printf("%s\n", log_file);
 
   /* Parse arguments. */
   parse_args(argc, argv);
