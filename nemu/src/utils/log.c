abbfde5437eab6048dff8f42999f0e33244e26a4
tracer-ics2023 <tracer@njuics.org>
2024-04-09 22:52:57 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:52:57 up  3:46,  6 users,  load average: 0.92, 1.29, 1.25

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -72,15 +72,24 @@ void ftrace_init(const char *ftrace_elf) {
   ftrace_symbols = fopen(symbols_path, "r");
   
   char ch;
-  char tmp[8] = {};
+  char tmp[9] = {};
   while((ch = fgetc(ftrace_symbols)) != EOF) {
     if (ch == ':') {
       fseek(ftrace_symbols, 1, SEEK_CUR);
-      // FILE *fp_addr = ftrace_symbols;
+      FILE *fp_addr = ftrace_symbols;
       fseek(ftrace_symbols, 15, SEEK_CUR);
       
       if (strcmp(fgets(tmp, 5, ftrace_symbols), "FUNC") == 0) {
-        printf("Goal! %s\n", tmp);
+        if (NULL != fgets(tmp, 9, fp_addr))
+          sscanf(tmp, "%x", &symbols[sptr].addr);
+
+        fseek(ftrace_symbols, 23, SEEK_CUR);
+        int i = 0;
+        while((ch = fgetc(ftrace_symbols)) != '\n') {
+          symbols[sptr].name[i++] = ch;
+        }
+        printf("%s: %#08x\n", symbols[sptr].name, symbols[sptr].addr);
+        sptr++;
       }
       
       // for (int i = 0; i < 4; i++) 
