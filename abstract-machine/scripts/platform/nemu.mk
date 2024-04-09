5f1616441bfaba39543386cef3de579d468ea8ef
tracer-ics2023 <tracer@njuics.org>
2024-04-09 20:57:59 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:57:59 up  1:51,  5 users,  load average: 0.63, 0.56, 0.61

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -29,4 +29,4 @@ gdb: image
 	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) gdb ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
 
 ftrace: image
-	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) gdb ARGS="$(NEMUFLAGS) -t $(IMAGE).elf" IMG=$(IMAGE).bin
+	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) gdb ARGS="-t $(IMAGE).elf $(NEMUFLAGS)" IMG=$(IMAGE).bin
