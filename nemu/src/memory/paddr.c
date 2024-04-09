3323ceaa097b6ad4b3daa556c278631e98ac3d23
tracer-ics2023 <tracer@njuics.org>
2024-04-09 15:41:02 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:41:02 up  1:17,  3 users,  load average: 0.48, 0.36, 0.55

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,7 +51,7 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  IFDEF(MTRACE_CONFUG, mtrace_read(addr, len));
+  IFDEF(MTRACE_CONFIG, mtrace_read(addr, len));
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -59,7 +59,7 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  IFDEF(MTRAC_CONFIG, mtrace_write(addr, data, len));
+  IFDEF(MTRACE_CONFIG, mtrace_write(addr, data, len));
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
