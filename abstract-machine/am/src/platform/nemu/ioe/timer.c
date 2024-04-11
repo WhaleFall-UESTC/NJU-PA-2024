248d5ec1d6102aae9e617f7d19da08fe8f465784
tracer-ics2023 <tracer@njuics.org>
2024-04-11 16:46:15 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:46:15 up  2:07,  2 users,  load average: 0.81, 0.62, 0.37

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -4,8 +4,8 @@
 static uint64_t boot_time = 0;
 
 uint64_t read_time() {
-  uint32_t lo = *(volatile uint32_t *) RTC_ADDR;
-  uint32_t hi = *(volatile uint32_t *)(RTC_ADDR + 4);
+  uint32_t lo = inl(RTC_ADDR);
+  uint32_t hi = inl(RTC_ADDR + 4);
   uint64_t cur_time = ((uint64_t)hi << 32) | lo;
   return cur_time / 10;
 }
