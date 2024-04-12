49b07997db03808b57f1186aa4fc644193fdd60c
tracer-ics2023 <tracer@njuics.org>
2024-04-12 20:01:29 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:01:29 up 12 min,  2 users,  load average: 0.89, 0.66, 0.45

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -2,15 +2,25 @@
 #include <nemu.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
+#define SIZE_MASK 0x0000ffff
 
 void __am_gpu_init() {
+  uint32_t s = inl(VGACTL_ADDR);
+  int w = (s >> 16) & SIZE_MASK;
+  int h = s & SIZE_MASK;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  int size = w * h;
+  for (int i = 0; i < size; i++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t s = inl(VGACTL_ADDR);
+  uint32_t w = (s >> 16) & SIZE_MASK, h = s & SIZE_MASK;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = w, .height = h,
+    .vmemsz = w * h
   };
 }
 
