af63dc58d7110f3ca3709622898bf5d270ef3d34
tracer-ics2023 <tracer@njuics.org>
2024-04-12 20:54:08 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:54:08 up  1:05,  3 users,  load average: 1.20, 1.08, 0.96

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -28,18 +28,19 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  if (w == 0 || h == 0) return;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   uint32_t *pixels = (uint32_t *)ctl->pixels;
-  uint32_t width = (inl(VGACTL_ADDR) >> 16) & SIZE_MASK;
+  uint32_t width = inl(VGACTL_ADDR) >> 16;
 
-  int base_fb = x * width + y;
-  int base_pixels = 0;
-  for (int i = 0; i < h; i++) {
-    for (int j = 0; j < w; j++) {
-      fb[base_fb + j] = pixels[base_pixels + j];
+  // int base_fb = x * width + y;
+  // int base_pixels = 0;
+  for (int i = y; i < y + h; i++) {
+    for (int j = x; j < x + w; j++) {
+      fb[x * width + j] = pixels[w * (i - y) + (j - x)];
     }
-    base_fb += width;
-    base_pixels += w;
+    // base_fb += width;
+    // base_pixels += w;
   }
 
   if (ctl->sync) {
