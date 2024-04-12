1633e4cae37c8aa74acda9f83d8ef80ab4f860ad
tracer-ics2023 <tracer@njuics.org>
2024-04-12 23:56:29 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:56:29 up 2 min,  2 users,  load average: 1.98, 0.83, 0.32

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -37,6 +37,7 @@ static uint32_t *audio_base = NULL;
 // }
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+  audio_base[reg_count] = 0;
   // // assert(!is_write);
   // // assert(offset == 0);
   
