31c2e23c13275619d007f69cf49f6710d2aee06a
tracer-ics2023 <tracer@njuics.org>
2024-04-12 20:49:09 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:49:09 up  1:00,  2 users,  load average: 2.08, 1.27, 0.96

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,8 +4,6 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 #define SIZE_MASK 0x0000ffff
 
-static uint32_t size = 0, width = 0, height = 0;
-
 void __am_gpu_init() {
   uint32_t wh = inl(VGACTL_ADDR);
   int w = (wh >> 16) & SIZE_MASK;
@@ -18,9 +16,9 @@ void __am_gpu_init() {
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   uint32_t wh = inl(VGACTL_ADDR);
-  width = (wh >> 16) & SIZE_MASK;
-  height = wh & SIZE_MASK;
-  size = width * height;
+  uint32_t width = (wh >> 16) & SIZE_MASK;
+  uint32_t height = wh & SIZE_MASK;
+  uint32_t size = width * height;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
     .width = width, .height = height,
@@ -32,6 +30,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   uint32_t *pixels = (uint32_t *)ctl->pixels;
+  uint32_t width = (inl(VGACTL_ADDR) >> 16) & SIZE_MASK;
 
   int base_fb = x * width + y;
   int base_pixels = 0;
