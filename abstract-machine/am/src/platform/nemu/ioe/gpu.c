5f6f637651d3705cda60ecd6ef595b131ebbb255
tracer-ics2023 <tracer@njuics.org>
2024-04-12 21:01:07 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:01:07 up  1:12,  3 users,  load average: 1.22, 1.22, 1.05

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -28,7 +28,7 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-  if (w == 0 || h == 0) return;
+  if (!ctl->sync && (w == 0 || h == 0)) return;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   uint32_t *pixels = (uint32_t *)ctl->pixels;
   uint32_t width = inl(VGACTL_ADDR) >> 16;
