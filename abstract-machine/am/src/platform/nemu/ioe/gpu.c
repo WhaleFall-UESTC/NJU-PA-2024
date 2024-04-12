1f682d2454123c47a13f5b285006de45e1fc1d12
tracer-ics2023 <tracer@njuics.org>
2024-04-12 20:20:51 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:20:51 up 32 min,  2 users,  load average: 0.20, 0.63, 0.62

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,29 +4,44 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 #define SIZE_MASK 0x0000ffff
 
+static uint32_t size = 0, width = 0, height = 0;
+
 void __am_gpu_init() {
-  uint32_t s = inl(VGACTL_ADDR);
-  int w = (s >> 16) & SIZE_MASK;
-  int h = s & SIZE_MASK;
+  uint32_t wh = inl(VGACTL_ADDR);
+  int w = (wh >> 16) & SIZE_MASK;
+  int h = wh & SIZE_MASK;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  int size = w * h;
-  for (int i = 0; i < size; i++) fb[i] = i;
+  int s = w * h;
+  for (int i = 0; i < s; i++) fb[i] = i;
   outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  uint32_t s = inl(VGACTL_ADDR);
-  uint32_t w = (s >> 16) & SIZE_MASK, h = s & SIZE_MASK;
+  uint32_t wh = inl(VGACTL_ADDR);
+  width = (wh >> 16) & SIZE_MASK;
+  height = wh & SIZE_MASK;
+  size = width * height;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = w, .height = h,
-    .vmemsz = w * h
+    .width = width, .height = height,
+    .vmemsz = size
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
+
+    uint32_t *start = (uint32_t *)(uintptr_t)FB_ADDR;
+    start += (ctl->x * width + ctl->y);
+    uint32_t *store = (uint32_t *)(ctl->pixels);
+    for (int i = 0; i < ctl->h; i++) {
+      for (int j = 0; j < ctl->w; j++) {
+        start[j] = store[j];
+      }
+      start += width;
+      store += ctl->w;
+    }
   }
 }
 
