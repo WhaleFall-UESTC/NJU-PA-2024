8043dcdf812189b5bb65ab32c05bd05ae2872762
tracer-ics2023 <tracer@njuics.org>
2024-04-14 22:45:39 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:45:39 up  1:37,  2 users,  load average: 1.24, 0.59, 0.56

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -28,7 +28,7 @@ void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = count;
+  stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
