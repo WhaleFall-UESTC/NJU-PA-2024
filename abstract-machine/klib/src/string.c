d82978b66ca167efaa527ae59212fe444fe1db41
tracer-ics2023 <tracer@njuics.org>
2024-04-15 09:28:54 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:28:54 up 21 min,  3 users,  load average: 1.35, 0.69, 0.49

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -55,9 +55,10 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  char _c = (char) c;
+  char _c  = (char)  c;
+  char *_s = (char *) s;
   for (int i = 0; i < n; i++)
-    ((char *)s)[i] = _c;
+    _s[i] = _c;
 
   return s;
 }
