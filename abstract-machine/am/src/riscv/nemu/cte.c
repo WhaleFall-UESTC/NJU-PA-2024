193255a6dee313684d1315eab12911c7b5734367
tracer-ics2023 <tracer@njuics.org>
2024-04-18 09:58:58 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:58:58 up  1:26,  3 users,  load average: 0.99, 1.05, 0.90

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -13,7 +13,7 @@ Context* __am_irq_handle(Context *c) {
 
     uint32_t *gpr = c->gpr;
     for (int i = 0; i < 16; i++) {
-      printf("%d: %d\t\t%d: %d\n", i, gpr[i], i + 16, gpr[i + 16]);
+      printf("%d: %x\t\t%d: %x\n", i, gpr[i], i + 16, gpr[i + 16]);
     }
     printf("mcause:%d\tmstatus:%d\tmepc:%d\n", c->mcause, c->mstatus, c->mepc);
 
