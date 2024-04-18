ffda8956635e906eb426ef2b37f64a7d8584651f
tracer-ics2023 <tracer@njuics.org>
2024-04-18 10:04:31 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:04:31 up  1:32,  3 users,  load average: 0.54, 0.90, 0.90

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,13 +8,14 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
+      case -1: ev.event = EVENT_YIELD; break;
       default: ev.event = EVENT_ERROR; break;
     }
 
-    for (int i = 0; i < 16; i++) {
-      printf("%d: %x\t\t%d: %x\n", i, c->gpr[i], i + 16, c->gpr[i + 16]);
-    }
-    printf("mcause:%d\tmstatus:%d\tmepc:%d\n", c->mcause, c->mstatus, c->mepc);
+    // for (int i = 0; i < 16; i++) {
+    //   printf("%d: %x\t\t%d: %x\n", i, c->gpr[i], i + 16, c->gpr[i + 16]);
+    // }
+    // printf("mcause:%d\tmstatus:%d\tmepc:%d\n", c->mcause, c->mstatus, c->mepc);
 
     c = user_handler(ev, c);
     assert(c != NULL);
