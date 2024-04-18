9207ab9a5d8685f7dddbf8d27d1e6f016f492b8a
whalefall <166845494@qq.com>
2024-04-18 15:12:02 +0800

    NJU-ProjectN/nanos-lite ics2021 initialized
    NJU-ProjectN/nanos-lite 2a141760e31be246a7316942293a97873925bc2f Makefile: use header files in newlib-navy

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,14 +1,7 @@
 #include <common.h>
 
-// enum {
-//     EVENT_NULL = 0,
-//     EVENT_YIELD, EVENT_SYSCALL, EVENT_PAGEFAULT, EVENT_ERROR,
-//     EVENT_IRQ_TIMER, EVENT_IRQ_IODEV,
-// } event;
-
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD: printf("Detected event YIELD\n"); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
