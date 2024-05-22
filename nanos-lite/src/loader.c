667765ccf0ad87682364008002ca93c983a4f74f
tracer-ics2023 <tracer@njuics.org>
2024-05-22 20:58:21 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:58:21 up  2:23,  5 users,  load average: 0.59, 1.01, 1.04

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -19,15 +19,23 @@
 #ifdef __LP64__
 #define Elf_Ehdr Elf64_Ehdr
 #define Elf_Phdr Elf64_Phdr
+#define Elf_Word Elf64_Word
+#define Elf_Half Elf64_Half
+#define Elf_Off Elf64_Off
+#define Elf_Addr Elf64_Addr
 #else
 #define Elf_Ehdr Elf32_Ehdr
 #define Elf_Phdr Elf32_Phdr
+#define Elf_Word Elf32_Word
+#define Elf_Half Elf32_Half
+#define Elf_Off Elf32_Off
+#define Elf_Addr Elf32_Addr
 #endif
 
 #define BUF 256
 
-// static void printEhdr(Elf_Ehdr ehdr);
-// static void printPhdr(Elf_Phdr phdr);
+static void printEhdr(Elf_Ehdr ehdr);
+static void printPhdr(Elf_Phdr phdr);
 
 static uintptr_t loader(PCB *pcb, const char *filename)
 {
@@ -38,36 +46,38 @@ static uintptr_t loader(PCB *pcb, const char *filename)
   printf("Why you 64?\n");
   #endif
 
-  uint16_t Ehdrsz = 0;
+  Elf_Half Ehdrsz = 0;
   ramdisk_read(&Ehdrsz, 28, 2);
   // printf("Ehdr: %d\tGet Size: %d\n", sizeof(Elf_Ehdr), Ehdrsz);
+  
   ramdisk_read(&ehdr, 0, Ehdrsz);
   assert(*((uint32_t *)(&ehdr.e_ident)) == 0x464c457f);
-  // printEhdr(ehdr);
-  uintptr_t entrypoint = (uintptr_t) ehdr.e_entry;
+  printEhdr(ehdr);
+  Elf_Addr entrypoint = (uintptr_t) ehdr.e_entry;
 
-  uint32_t e_phoff = ehdr.e_phoff;
-  uint16_t e_phentsize = ehdr.e_phentsize;
-  uint16_t e_phnum = ehdr.e_phnum;
+  Elf_Off e_phoff = ehdr.e_phoff;
+  Elf_Half e_phentsize = ehdr.e_phentsize;
+  Elf_Half e_phnum = ehdr.e_phnum;
 
-  uint32_t base = e_phoff + e_phnum * e_phentsize;
+  Elf_Addr base = e_phoff + e_phnum * e_phentsize;
 
 
   for (int i = 0; i < e_phnum; i++)
   {
     ramdisk_read(&phdr, e_phoff + i * e_phentsize, e_phentsize);
     
-    if ((uint16_t)phdr.p_type != PT_LOAD)
+    if ((Elf_Half)phdr.p_type != PT_LOAD)
       continue;
     else {
-      // printf("\nLoad this segment\n");
-      // printPhdr(phdr);
+      printf("\nLoad this segment\n");
+      printPhdr(phdr);
     }
 
     char buf_tmp[BUF];
-    uint32_t filesz = phdr.p_filesz, offset = phdr.p_offset;
-    uint32_t nread = filesz, read = 0;
-    uintptr_t vaddr = phdr.p_vaddr;
+    Elf_Word filesz = phdr.p_filesz;
+    Elf_Off offset = phdr.p_offset;
+    Elf_Word nread = filesz, read = 0;
+    Elf_Addr vaddr = phdr.p_vaddr;
     while (nread)
     {
       read = (nread < BUF) ? nread : BUF;
@@ -81,7 +91,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)
     memset((void *)vaddr, 0, phdr.p_memsz - filesz);
   }
 
-  return (uintptr_t) entrypoint;
+  return (Elf_Addr) entrypoint;
 }
 
 void naive_uload(PCB *pcb, const char *filename)
@@ -92,30 +102,30 @@ void naive_uload(PCB *pcb, const char *filename)
 }
 
 
-// static void printEhdr(Elf_Ehdr ehdr) {
-//   printf("magic = %#08x\n", *((uint32_t *)(&ehdr.e_ident)));
-//   printf("e_type = %#08x\n", ehdr.e_type);
-//   printf("e_machine = %#08x\n", ehdr.e_machine);
-//   printf("e_version = %#08x\n", ehdr.e_version);
-//   printf("e_entry = %#08x\n", ehdr.e_entry);
-//   printf("e_phoff = %#08x\n", ehdr.e_phoff);
-//   printf("e_shoff = %#08x\n", ehdr.e_shoff);
-//   printf("e_flags = %#08x\n", ehdr.e_flags);
-//   printf("e_ehsize = %#08x\n", ehdr.e_ehsize);
-//   printf("e_phentsize = %#08x\n", ehdr.e_phentsize);
-//   printf("e_phnum = %#08x\n", ehdr.e_phnum);
-//   printf("e_shentsize = %#08x\n", ehdr.e_shentsize);
-//   printf("e_shnum = %#08x\n", ehdr.e_shnum);
-//   printf("e_shstrndx = %#08x\n", ehdr.e_shstrndx);
-// }
+static void printEhdr(Elf_Ehdr ehdr) {
+  printf("magic = %#08x\n", *((uint32_t *)(&ehdr.e_ident)));
+  printf("e_type = %#08x\n", ehdr.e_type);
+  printf("e_machine = %#08x\n", ehdr.e_machine);
+  printf("e_version = %#08x\n", ehdr.e_version);
+  printf("e_entry = %#08x\n", ehdr.e_entry);
+  printf("e_phoff = %#08x\n", ehdr.e_phoff);
+  printf("e_shoff = %#08x\n", ehdr.e_shoff);
+  printf("e_flags = %#08x\n", ehdr.e_flags);
+  printf("e_ehsize = %#08x\n", ehdr.e_ehsize);
+  printf("e_phentsize = %#08x\n", ehdr.e_phentsize);
+  printf("e_phnum = %#08x\n", ehdr.e_phnum);
+  printf("e_shentsize = %#08x\n", ehdr.e_shentsize);
+  printf("e_shnum = %#08x\n", ehdr.e_shnum);
+  printf("e_shstrndx = %#08x\n", ehdr.e_shstrndx);
+}
 
-// static void printPhdr(Elf_Phdr phdr) {
-//   printf("p_type = %#08x\n", phdr.p_type);
-//   printf("p_offset = %#08x\n", phdr.p_offset);
-//   printf("p_vaddr = %#08x\n", phdr.p_vaddr);
-//   printf("p_paddr = %#08x\n", phdr.p_paddr);
-//   printf("p_filesz = %#08x\n", phdr.p_filesz);
-//   printf("p_memsz = %#08x\n", phdr.p_memsz);
-//   printf("p_flags = %#08x\n", phdr.p_flags);
-//   printf("p_align = %#08x\n", phdr.p_align);
-// }
+static void printPhdr(Elf_Phdr phdr) {
+  printf("p_type = %#08x\n", phdr.p_type);
+  printf("p_offset = %#08x\n", phdr.p_offset);
+  printf("p_vaddr = %#08x\n", phdr.p_vaddr);
+  printf("p_paddr = %#08x\n", phdr.p_paddr);
+  printf("p_filesz = %#08x\n", phdr.p_filesz);
+  printf("p_memsz = %#08x\n", phdr.p_memsz);
+  printf("p_flags = %#08x\n", phdr.p_flags);
+  printf("p_align = %#08x\n", phdr.p_align);
+}
