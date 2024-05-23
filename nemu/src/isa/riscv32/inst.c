28a9a45f56f8bbcc70f3fc4b2b81539b54b545f4
tracer-ics2023 <tracer@njuics.org>
2024-05-23 20:28:13 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:28:13 up  1:26,  3 users,  load average: 0.36, 0.65, 0.73

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -23,7 +23,6 @@
 #define Mr vaddr_read
 #define Mw vaddr_write
 
-// word_t ecall(word_t sys_call, vaddr_t epc);
 #define ECALL(dnpc) do { dnpc = (isa_raise_intr(R(isa_reg_str2val("a7", NULL)), s->pc)); } while(0)
 word_t get_trap_csr(int i);
 void set_trap_csr(int i, word_t value);
