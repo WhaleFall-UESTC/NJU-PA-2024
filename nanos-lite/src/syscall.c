90fd786d3d34f471d449cfa705cc633590a35c3f
tracer-ics2023 <tracer@njuics.org>
2024-05-23 22:35:01 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:35:01 up  3:33,  2 users,  load average: 0.54, 0.49, 0.48

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -17,6 +17,16 @@ void do_syscall(Context *c) {
     case SYS_exit:  printf("0\n"); c->GPRx = 0; halt(0); break;
     case SYS_yield:  printf("0\n"); c->GPRx = 0; yield(); break;
 
+    case SYS_write: {
+      char *buf = (char *) a[2];
+      if (a[1] == 1 || a[1] == 2) {
+        for (int i = 0; i < a[3]; i++) putch(buf[i]);
+      } else {
+        
+      }
+      printf("%d\n", a[3]); c->GPRx = a[3]; break;
+    }
+
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
