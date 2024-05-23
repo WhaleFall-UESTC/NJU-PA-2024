a830ab70da1863a04196275d5ccba3c966df174a
tracer-ics2023 <tracer@njuics.org>
2024-05-23 23:05:36 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:05:36 up  4:04,  3 users,  load average: 1.14, 1.06, 0.85

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -18,7 +18,8 @@ void do_syscall(Context *c) {
     case SYS_yield:  c->GPRx = 0; yield(); break;
 
     case SYS_write: {
-      printf("%c", *((char *)a[2]));
+      printf("shit");
+      // printf("%c", *((char *)a[2]));
       // if (a[1] == 1 || a[1] == 2) {
       //   // for (int i = 0; i < a[3]; i++) 
       //   printf("%c", *((char *)a[2]));
