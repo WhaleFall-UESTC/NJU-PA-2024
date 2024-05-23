a5c3f2eebcf0af1ef2e77ffee3e6d98d7306d970
tracer-ics2023 <tracer@njuics.org>
2024-05-23 23:01:26 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:01:26 up  4:00,  3 users,  load average: 0.95, 0.72, 0.71

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,7 +12,7 @@ void do_syscall(Context *c) {
   a[3] = c->GPR4;
 
   printf("Syscall ID = %02d\t, Arguments: %2d, %#08x, %2d\tRet: ", a[0], a[1], a[2], a[3]);
-
+  printf("%c", *((char *)a[2]));
   switch (a[0]) {
     case SYS_exit:  printf("0\n"); c->GPRx = 0; halt(0); break;
     case SYS_yield:  printf("0\n"); c->GPRx = 0; yield(); break;
