225b658cabe74b05fa2d06923df780ec6576aa24
tracer-ics2023 <tracer@njuics.org>
2024-05-23 23:03:23 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:03:23 up  4:02,  3 users,  load average: 1.04, 0.82, 0.74

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -19,7 +19,8 @@ void do_syscall(Context *c) {
 
     case SYS_write: {
       if (a[1] == 1 || a[1] == 2) {
-        for (int i = 0; i < a[3]; i++) putch(*((char *)(a[2])));
+        // for (int i = 0; i < a[3]; i++) 
+        printf("%c", *((char *)a[2]));
       } else {
         
       }
