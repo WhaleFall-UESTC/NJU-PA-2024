ed59752306f13081150a43f7201c7ced6744db78
tracer-ics2023 <tracer@njuics.org>
2024-05-23 21:58:30 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:58:30 up  2:57,  2 users,  load average: 2.46, 1.77, 1.23

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,8 +4,8 @@ void do_syscall(Context *c);
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case 1: printf("Detect EVENT_YIELD\n"); break;
-    case 2: printf("Detect EVENT_SYSCALL\n"); do_syscall(c); break;
+    case 1: /*printf("Detect EVENT_YIELD\n");*/ break;
+    case 2: /*printf("Detect EVENT_SYSCALL\n");*/ do_syscall(c); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,9 +12,11 @@ void do_syscall(Context *c) {
   a[3] = c->GPR4;
 
   switch (a[0]) {
-    case SYS_exit:  printf("SYS_exit\n"); c->GPRx = 0; halt(0); break;
-    case SYS_yield:  printf("SYS_yield\n"); c->GPRx = 0; yield(); break;
+    case SYS_exit:  /*printf("SYS_exit\n");*/ c->GPRx = 0; halt(0); break;
+    case SYS_yield:  /*printf("SYS_yield\n");*/ c->GPRx = 0; yield(); break;
 
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+
+  printf("Syscall ID = %02d\t, Arguments: %2d, %2d, %2d\t, Ret :%d\n", a[0], a[1], a[2], a[3], c->GPRx);
 }
