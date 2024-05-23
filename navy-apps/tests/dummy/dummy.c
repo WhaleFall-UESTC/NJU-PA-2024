859dfadd41bffec7f58f68df8a69d1f2214e3cd4
tracer-ics2023 <tracer@njuics.org>
2024-05-23 20:21:35 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:21:35 up  1:20,  2 users,  load average: 0.79, 0.85, 0.81

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -11,7 +11,7 @@ extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 int main()
 {
   // puts("This is dummy");
-  return _syscall_(SYS_yield, 1, 1, 1);
-  // asm volatile("li a7, 1; ecall");
+  // return _syscall_(SYS_yield, 1, 1, 1);
+  asm volatile("li a7, 1; ecall");
   // return 0;
 }
