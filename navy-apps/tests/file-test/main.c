84d81aff076093d30c69cc3382063fcd6e6089d7
tracer-ics2023 <tracer@njuics.org>
2024-05-25 16:01:05 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:01:05 up  1:37,  3 users,  load average: 0.71, 0.49, 0.68

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -18,16 +18,16 @@ int main() {
   }
   printf("Pass s2\n");
 
-  // fseek(fp, 0, SEEK_SET);
-  // for (i = 0; i < 500; i ++) {
-  //   fprintf(fp, "%4d\n", i + 1 + 1000);
-  // }
+  fseek(fp, 0, SEEK_SET);
+  for (i = 0; i < 500; i ++) {
+    fprintf(fp, "%4d\n", i + 1 + 1000);
+  }
 
-  // for (i = 500; i < 1000; i ++) {
-  //   fscanf(fp, "%d", &n);
-  //   assert(n == i + 1);
-  // }
-  // printf("Pass s3\n");
+  for (i = 500; i < 1000; i ++) {
+    fscanf(fp, "%d", &n);
+    assert(n == i + 1);
+  }
+  printf("Pass s3\n");
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
