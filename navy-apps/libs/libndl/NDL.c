c80d0f909172c2b1dd06ce684e81dfeb0ebf1fdd
tracer-ics2023 <tracer@njuics.org>
2024-05-26 00:35:00 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:35:00 up 3 min,  2 users,  load average: 1.14, 1.01, 0.46

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -97,6 +97,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h)
     lseek(fbdev, ((y + canvas_y + i) * screen_w + (x + canvas_x)) * 4, SEEK_SET);
     write(fbdev, pixels + i * w, 4 * (w < canvas_x - x ? w : canvas_w - x));
   }
+  free(pixels);
   assert(close(fbdev) == 0);
 }
 
--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -9,8 +9,11 @@ int main() {
   int w, h;
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
   assert(bmp);
+  printf("Load\n");
   NDL_OpenCanvas(&w, &h);
+  printf("Open\n");
   NDL_DrawRect(bmp, 0, 0, w, h);
+  printf("Draw\n");
   free(bmp);
   NDL_Quit();
   printf("Test ends! Spinning...\n");
