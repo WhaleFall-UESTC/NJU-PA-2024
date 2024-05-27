dcd5dd8f171290fe0bc4acae24ad61133c5bad7b
tracer-ics2023 <tracer@njuics.org>
2024-05-27 15:39:04 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:39:04 up 26 min,  2 users,  load average: 1.85, 0.96, 0.84

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,6 +15,30 @@ static const char *keyname[256] __attribute__((used)) = {
   AM_KEYS(NAME)
 };
 
+static bool has_uart = 0;
+static bool has_key = 0;
+
+AM_INPUT_KEYBRD_T keybrd_queue[256];
+int keybrd_head = 0, keybrd_tail = 0;
+
+void update_keybrd() {
+  if (has_key) {
+    while(1) {
+      AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
+      if (ev.keycode != AM_KEY_NONE) {
+        // if (ev.keydown) {}
+        keybrd_queue[keybrd_tail++] = ev;
+        keybrd_tail %= 256;
+        if (keybrd_tail == keybrd_head)
+          keybrd_head = (keybrd_head + 1) % 256;
+      }
+      else {
+        break;
+      }
+    }
+  }
+}
+
 size_t serial_write(const void *buf, size_t offset, size_t len) {
   for (size_t i = 0; i < len; i++)
     putch(*((char *)(buf + i)));
@@ -22,13 +46,22 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  printf("Call events_read\n");
-  AM_INPUT_KEYBRD_T in = io_read(AM_INPUT_KEYBRD);
-  if (in.keycode == AM_KEY_NONE) {
-    *(char *)buf = '\0';
-    return 0;
+  // AM_INPUT_KEYBRD_T in = io_read(AM_INPUT_KEYBRD);
+  // if (in.keycode == AM_KEY_NONE) {
+  //   *(char *)buf = '\0';
+  //   return 0;
+  // }
+  // return snprintf((char *)buf, len, "%s %s\n", in.keydown ? "kd" : "ku", keyname[in.keydown]);
+  if (has_uart);
+  if (has_key) {
+    if (keybrd_head != keybrd_tail) {
+      AM_INPUT_KEYBRD_T ev = keybrd_queue[keybrd_head++];
+      keybrd_head %= 256;
+      return snprintf((char *)buf, len, "%s %s\n", ev.keydown ? "kd" : "ku", keyname[ev.keycode]);
+    }
   }
-  return snprintf((char *)buf, len, "%s %s\n", in.keydown ? "kd" : "ku", keyname[in.keydown]);
+
+  return 0;
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
@@ -53,4 +86,5 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 void init_device() {
   Log("Initializing devices...");
   ioe_init();
+  has_key = io_read(AM_INPUT_CONFIG).present;
 }
