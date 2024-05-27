bce20cde16c76230d91ae6681983f312de4fe5cc
tracer-ics2023 <tracer@njuics.org>
2024-05-27 20:44:05 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:44:05 up 22 min,  3 users,  load average: 0.69, 1.12, 0.93

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -4,10 +4,12 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <assert.h>
 
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
+static int canvas_x = 0, canvas_y = 0;
 
 static uint32_t timer_start = 0;
 uint32_t NDL_GetTicks() {
@@ -18,10 +20,14 @@ uint32_t NDL_GetTicks() {
 
 int NDL_PollEvent(char *buf, int len) {
   int fd = open("/dev/events", 0, 0);
-  return read(fd, buf, len);
+  int ret = read(fd, buf, len);
+  assert(close(fd) == 0);
+  return ret;
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
+  if (*w == 0 || *w > screen_w) *w = screen_w;
+  if (*h == 0 || *h > screen_h) *h = screen_h;
   if (getenv("NWM_APP")) {
     int fbctl = 4;
     fbdev = 5;
@@ -38,10 +44,19 @@ void NDL_OpenCanvas(int *w, int *h) {
       if (strcmp(buf, "mmap ok") == 0) break;
     }
     close(fbctl);
+  } else {
+    canvas_x = (screen_w - *w) / 2;
+    canvas_y = (screen_h - *h) / 2;
   }
 }
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
+  int fd = open("/dev/fb", 0, 0);
+  for (int i = 0; i < h; i++) {
+    lseek(fd, ((canvas_y + y + i) * screen_w + x + canvas_x) * 4, SEEK_SET);
+    write(fd, pixels + i * w, w * 4);
+  }
+  assert(close(fd) == 0);
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {
@@ -58,15 +73,44 @@ int NDL_QueryAudio() {
   return 0;
 }
 
+void init_ticks() {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  timer_start = ((uint32_t)tv.tv_sec) * 1000 + tv.tv_usec / 1000;
+}
+
+void init_display() {
+  int fd = open("/proc/dispinfo", 0, 0);
+  char buf[64];
+  int nread = read(fd, buf, sizeof(buf));
+  assert(nread > 0 && nread < sizeof(buf));
+
+  char *w_s = strstr(buf, "WIDTH");
+  char *h_s = strstr(buf, "HEIGHT");
+
+  if (w_s) {
+    w_s = strchr(w_s, ':') + 1;
+    screen_w = atoi(w_s);
+  }
+
+  if (h_s) {
+    h_s = strchr(h_s, ':') + 1;
+    screen_h = atoi(h_s);
+  }
+
+  assert(close(fd) == 0);
+  printf("Get screen size %dx%d\n", screen_w, screen_h);
+}
+
 int NDL_Init(uint32_t flags) {
   if (getenv("NWM_APP")) {
     evtdev = 3;
   }
+
+  init_ticks();
+  init_display();
   return 0;
 }
 
 void NDL_Quit() {
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  timer_start = ((uint32_t)tv.tv_sec) * 1000 + tv.tv_usec / 1000;
 }
