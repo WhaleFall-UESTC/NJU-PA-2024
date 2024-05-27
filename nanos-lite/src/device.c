33d62eb75360c2a7b0bde1b930acdfb1c00c973e
tracer-ics2023 <tracer@njuics.org>
2024-05-27 15:06:23 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:06:23 up 22 min,  2 users,  load average: 0.60, 0.98, 0.72

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -22,6 +22,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
+  printf("Call events_read\n");
   AM_INPUT_KEYBRD_T in = io_read(AM_INPUT_KEYBRD);
   if (in.keycode == AM_KEY_NONE) {
     *(char *)buf = '\0';
