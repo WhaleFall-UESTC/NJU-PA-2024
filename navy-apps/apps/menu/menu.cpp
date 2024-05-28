405490ea3da957af712e9c6f064e9499a3c2cfc7
tracer-ics2023 <tracer@njuics.org>
2024-05-28 19:35:57 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:35:57 up 21 min,  2 users,  load average: 0.62, 0.67, 0.61

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -72,11 +72,14 @@ int main(int argc, char *argv[], char *envp[]) {
   screen = SDL_SetVideoMode(0, 0, 32, SDL_HWSURFACE);
 
   font = new BDF_Font(font_fname);
+  printf("Load BMP\n");
   logo_sf = SDL_LoadBMP("/share/pictures/projectn.bmp");
   assert(logo_sf);
+  printf("Finish loading\n");
   set_i_max();
 
   while (1) {
+    printf("Display menu\n");
     display_menu(i_max);
 
     SDL_Event e;
