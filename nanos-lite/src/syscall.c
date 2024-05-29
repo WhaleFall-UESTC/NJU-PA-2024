500886dbeb20583aa2c143f86eb60bb2ae3a6c87
tracer-ics2023 <tracer@njuics.org>
2024-05-29 16:18:17 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:18:17 up 20 min,  2 users,  load average: 1.80, 0.78, 0.91

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -20,7 +20,7 @@ void do_syscall(Context *c) {
       int mode = c->GPR4;
 
       c->GPRx = fs_open(path, flags, mode);
-      // Log("sys open %s, fd = %d", path, c->GPRx);
+      Log("sys open %s, fd = %d", path, c->GPRx);
       break;
     }
 
@@ -30,7 +30,7 @@ void do_syscall(Context *c) {
       int len = c->GPR4;
 
       c->GPRx = fs_write(fd, buf, len);
-      // Log("sys write to fd:%d %s, len = %d", fd, get_filename(fd), len);
+      Log("sys write to fd:%d %s, len = %d", fd, get_filename(fd), len);
       break;
     }
 
@@ -40,7 +40,7 @@ void do_syscall(Context *c) {
       int len = c->GPR4;
 
       c->GPRx = fs_read(fd, buf, len);
-      Log("sys read from fd: %d, len = %d", fd, len);
+      Log("sys read from fd: %d %s, len = %d", fd, get_filename(fd), len);
       break;
     }
 
@@ -50,7 +50,7 @@ void do_syscall(Context *c) {
       int whence = c->GPR4;
 
       c->GPRx = fs_lseek(fd, offset, whence);
-      // Log("sys write to fd:%d %s, offset switch to %d", fd, get_filename(fd), c->GPRx);
+      Log("sys lseek fd:%d %s, offset switch to %d", fd, get_filename(fd), c->GPRx);
       break;
     }
 
