12ac323de168458169e686f674c79552b27bbe0c
tracer-ics2023 <tracer@njuics.org>
2024-05-31 11:23:29 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:23:29 up 35 min,  4 users,  load average: 2.29, 1.05, 0.83

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -42,8 +42,8 @@
 
 #define BUF 256
 
-static void printEhdr(Elf_Ehdr ehdr);
-static void printPhdr(Elf_Phdr phdr);
+// static void printEhdr(Elf_Ehdr ehdr);
+// static void printPhdr(Elf_Phdr phdr);
 
 static uintptr_t loader(PCB *pcb, const char *filename)
 {
@@ -56,7 +56,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)
 
   int fd = fs_open(filename, 0, 0);
   fs_read(fd, &ehdr, sizeof(Elf_Ehdr));
-  printEhdr(ehdr);
+  // printEhdr(ehdr);
   // ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
   assert(*((uint32_t *)(&ehdr.e_ident)) == 0x464c457f);
   Elf_Addr entrypoint = ehdr.e_entry;
@@ -74,7 +74,7 @@ static uintptr_t loader(PCB *pcb, const char *filename)
     if ((Elf_Word)phdr.p_type != PT_LOAD)
       continue;
     
-    printPhdr(phdr);
+    // printPhdr(phdr);
 
     // ramdisk_read((void *)phdr.p_vaddr, phdr.p_offset, phdr.p_memsz);
     fs_lseek(fd, phdr.p_offset, SEEK_SET);
@@ -93,30 +93,30 @@ void naive_uload(PCB *pcb, const char *filename)
 }
 
 
-static void printEhdr(Elf_Ehdr ehdr) {
-  printf("magic = %#08x\n", *((uint32_t *)(&ehdr.e_ident)));
-  printf("e_type = %#08x\n", ehdr.e_type);
-  printf("e_machine = %#08x\n", ehdr.e_machine);
-  printf("e_version = %#08x\n", ehdr.e_version);
-  printf("e_entry = %#08x\n", ehdr.e_entry);
-  printf("e_phoff = %#08x\n", ehdr.e_phoff);
-  printf("e_shoff = %#08x\n", ehdr.e_shoff);
-  printf("e_flags = %#08x\n", ehdr.e_flags);
-  printf("e_ehsize = %d\n", ehdr.e_ehsize);
-  printf("e_phentsize = %d\n", ehdr.e_phentsize);
-  printf("e_phnum = %d\n", ehdr.e_phnum);
-  printf("e_shentsize = %d\n", ehdr.e_shentsize);
-  printf("e_shnum = %d\n", ehdr.e_shnum);
-  printf("e_shstrndx = %#08x\n", ehdr.e_shstrndx);
-}
+// static void printEhdr(Elf_Ehdr ehdr) {
+//   printf("magic = %#08x\n", *((uint32_t *)(&ehdr.e_ident)));
+//   printf("e_type = %#08x\n", ehdr.e_type);
+//   printf("e_machine = %#08x\n", ehdr.e_machine);
+//   printf("e_version = %#08x\n", ehdr.e_version);
+//   printf("e_entry = %#08x\n", ehdr.e_entry);
+//   printf("e_phoff = %#08x\n", ehdr.e_phoff);
+//   printf("e_shoff = %#08x\n", ehdr.e_shoff);
+//   printf("e_flags = %#08x\n", ehdr.e_flags);
+//   printf("e_ehsize = %d\n", ehdr.e_ehsize);
+//   printf("e_phentsize = %d\n", ehdr.e_phentsize);
+//   printf("e_phnum = %d\n", ehdr.e_phnum);
+//   printf("e_shentsize = %d\n", ehdr.e_shentsize);
+//   printf("e_shnum = %d\n", ehdr.e_shnum);
+//   printf("e_shstrndx = %#08x\n", ehdr.e_shstrndx);
+// }
 
-static void printPhdr(Elf_Phdr phdr) {
-  printf("p_type = %#08x\n", phdr.p_type);
-  printf("p_offset = %#08x\n", phdr.p_offset);
-  printf("p_vaddr = %#08x\n", phdr.p_vaddr);
-  printf("p_paddr = %#08x\n", phdr.p_paddr);
-  printf("p_filesz = %#08x\n", phdr.p_filesz);
-  printf("p_memsz = %#08x\n", phdr.p_memsz);
-  printf("p_flags = %#08x\n", phdr.p_flags);
-  printf("p_align = %#08x\n", phdr.p_align);
-}
+// static void printPhdr(Elf_Phdr phdr) {
+//   printf("p_type = %#08x\n", phdr.p_type);
+//   printf("p_offset = %#08x\n", phdr.p_offset);
+//   printf("p_vaddr = %#08x\n", phdr.p_vaddr);
+//   printf("p_paddr = %#08x\n", phdr.p_paddr);
+//   printf("p_filesz = %#08x\n", phdr.p_filesz);
+//   printf("p_memsz = %#08x\n", phdr.p_memsz);
+//   printf("p_flags = %#08x\n", phdr.p_flags);
+//   printf("p_align = %#08x\n", phdr.p_align);
+// }
