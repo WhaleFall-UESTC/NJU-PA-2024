6ad2faa04af63035c29df6007411834b0029a8e7
tracer-ics2023 <tracer@njuics.org>
2024-05-31 10:45:43 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:45:43 up 21 min,  4 users,  load average: 1.93, 1.50, 1.03

---
+++ nemu/src/isa/riscv64/system/intr.c
@@ -0,0 +1,78 @@
+/***************************************************************************************
+* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+*
+* NEMU is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+#include <isa.h>
+#include <utils.h>
+
+
+// enum {
+//   mstatus, misa, meedleg, mideleg, mie, mtvec, mcounteren, mstatush, 
+//   mscratch, mepc, mcause, mtval, mip, mtinst, mtval12
+// } Machine_Trap;
+
+// word_t trap_csr[15] = {};
+
+// #define CSRs(csr) trap_csr[csr < 15 ? csr : addr2csr(csr)]
+
+// int addr2csr(word_t addr) {
+//   if (addr >= 0x300 && addr <= 0x306) return addr - 0x300;
+//   if (addr >= 0x340 && addr <= 0x344) return addr - 0x340 + 8;
+//   switch (addr) {
+//     case 0x310: return 7;
+//     case 0x34a: return 13;
+//     case 0x34b: return 14;
+//     default: return -1;
+//   }
+// }
+
+enum { mepc, mcause, mstatus, mtvec };
+word_t trap_csr[4] = {};
+
+void set_trap_csr(int i, word_t value) { trap_csr[i] = value; }
+word_t get_trap_csr(int i) { return trap_csr[i]; }
+
+
+int csr_register(word_t imm) {
+  switch (imm) {
+    case 0x341: return mepc; //&(cpu.csr.mepc);
+    case 0x342: return mcause; //&(cpu.csr.mcause);
+    case 0x300: return mstatus; //&(cpu.csr.mstatus);
+    case 0x305: return mtvec; //&(cpu.csr.mtvec);
+    default: panic("unknown csr");
+  }
+}
+
+word_t ecall(word_t sys_call, vaddr_t epc) {
+  switch(sys_call) {
+    case -1: return isa_raise_intr(1, epc); // EVENT_YIELD
+    default: return isa_raise_intr(sys_call, epc);
+  }
+}
+
+void etrace_log(int, word_t);
+
+word_t isa_raise_intr(word_t NO, vaddr_t epc) {
+  /* TODO: Trigger an interrupt/exception with ``NO''.
+   * Then return the address of the interrupt/exception vector.
+   */
+  etrace_log(NO, epc);
+  trap_csr[mcause] = NO;
+  trap_csr[mepc] = epc;
+  return trap_csr[mtvec];
+}
+
+word_t isa_query_intr() {
+  return INTR_EMPTY;
+}
