0022def95c014a672aba1dcbfb14fb5cf702c1dd
tracer-ics2023 <tracer@njuics.org>
2024-05-31 11:21:32 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:21:32 up 33 min,  4 users,  load average: 0.17, 0.30, 0.59

---
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -0,0 +1,59 @@
+#include <NDL.h>
+#include <SDL.h>
+#include <assert.h>
+#include <string.h>
+
+#define keyname(k) #k,
+
+#define def_keystate(k) [k] = 1,
+
+static const char *keyname[] = {
+  "NONE",
+  _KEYS(keyname)
+};
+
+static uint8_t keystate[SDLK_LAST];
+
+static inline uint8_t find_keyname(char *name) {
+  strtok(name, "\n");
+  for (int i = 0; i < sizeof(keyname) / sizeof(keyname[0]); i++) {
+    if (strcmp(keyname[i], name) == 0) {
+      return i;
+    }
+  }
+  assert(0);
+  return 0;
+}
+
+int SDL_PushEvent(SDL_Event *ev) {
+  assert(0);
+  return 0;
+}
+
+int SDL_PollEvent(SDL_Event *ev) {
+  InvokeAudioCallback();
+  if (ev == NULL)
+    return 0;
+  char buf[32];
+  int ret = NDL_PollEvent(buf, sizeof(buf));
+  if (ret) {
+    ev->type = buf[1] == 'u' ? SDL_KEYUP : SDL_KEYDOWN;
+    ev->key.keysym.sym = find_keyname(buf + 3);
+    keystate[ev->key.keysym.sym] = ev->type == SDL_KEYDOWN;
+  }
+  return ret;
+}
+
+int SDL_WaitEvent(SDL_Event *event) {
+  while (SDL_PollEvent(event) == 0);
+  return 1;
+}
+
+int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
+  assert(0);
+  return 0;
+}
+
+uint8_t* SDL_GetKeyState(int *numkeys) {
+  return keystate;
+}
