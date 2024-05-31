ffd533d09b304a0782d79e0d7cbc904d304ae6d4
tracer-ics2023 <tracer@njuics.org>
2024-05-31 22:18:10 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:18:10 up  1:24,  3 users,  load average: 1.10, 0.80, 0.51

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -31,7 +31,7 @@ void init_proc() {
 }
 
 Context* schedule(Context *prev) {
-  Log("Switch to process");
+  // Log("Switch process");
   current->cp = prev;
   current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   return current->cp;
