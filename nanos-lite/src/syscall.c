c9d78d25546fbcb7087d774c610f8e24c14bd45f
tracer-ics2023 <tracer@njuics.org>
2024-06-02 10:03:02 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:03:02 up 8 min,  2 users,  load average: 1.63, 1.58, 0.77

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -17,7 +17,8 @@ void do_syscall(Context *c) {
       // switch_boot_pcb();
       // yield();
       // assert(0); 
-      halt(c->GPR2);
+      // halt(c->GPR2);
+      c->GPRx = 0;
       break;
     case SYS_yield:  c->GPRx = 0; yield(); break;
 
@@ -27,7 +28,7 @@ void do_syscall(Context *c) {
       int mode = c->GPR4;
 
       c->GPRx = fs_open(path, flags, mode);
-      Log("sys open %s, fd = %d", path, c->GPRx);
+      // Log("sys open %s, fd = %d", path, c->GPRx);
       break;
     }
 
@@ -37,7 +38,7 @@ void do_syscall(Context *c) {
       int len = c->GPR4;
 
       c->GPRx = fs_write(fd, buf, len);
-      Log("sys write to fd:%d %s, len = %d", fd, get_filename(fd), len);
+      // Log("sys write to fd:%d %s, len = %d", fd, get_filename(fd), len);
       break;
     }
 
@@ -47,7 +48,7 @@ void do_syscall(Context *c) {
       int len = c->GPR4;
 
       c->GPRx = fs_read(fd, buf, len);
-      Log("sys read from fd: %d %s, len = %d", fd, get_filename(fd), len);
+      // Log("sys read from fd: %d %s, len = %d", fd, get_filename(fd), len);
       break;
     }
 
@@ -57,7 +58,7 @@ void do_syscall(Context *c) {
       int whence = c->GPR4;
 
       c->GPRx = fs_lseek(fd, offset, whence);
-      Log("sys lseek fd:%d %s, offset switch to %d", fd, get_filename(fd), c->GPRx);
+      // Log("sys lseek fd:%d %s, offset switch to %d", fd, get_filename(fd), c->GPRx);
       break;
     }
 
