789f660e71cdca9ba9659c6a28b07d492b832667
tracer-ics2023 <tracer@njuics.org>
2024-06-02 10:18:35 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:18:35 up 12 min,  2 users,  load average: 1.84, 0.77, 0.42

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,13 +13,13 @@ void do_syscall(Context *c) {
   
   switch (a[0]) {
     case SYS_exit:  
-      context_uload(current, "/bin/nterm", (char* const[]){"/bin/nterm", NULL}, (char* const[]){NULL});
-      switch_boot_pcb();
-      yield();
-      assert(0); 
+      // context_uload(current, "/bin/nterm", (char* const[]){"/bin/nterm", NULL}, (char* const[]){NULL});
+      // switch_boot_pcb();
+      // yield();
+      // assert(0); 
       // halt(c->GPR2);
-      // c->GPRx = 0;
-      // break;
+      c->GPRx = 0;
+      break;
     case SYS_yield:  c->GPRx = 0; yield(); break;
 
     case SYS_open: {
