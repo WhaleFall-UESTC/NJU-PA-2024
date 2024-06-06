dac6206ff7abdaac61abb57b0d8bddf6df3a7288
tracer-ics2023 <tracer@njuics.org>
2024-06-06 19:58:22 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:58:22 up 20 min,  2 users,  load average: 1.51, 0.93, 0.72

--- navy-apps/apps/nterm/src/main.cpp
+++ navy-apps/apps/nterm/src/main.cpp
@@ -11,13 +11,17 @@ void builtin_sh_run();
 void extern_app_run(const char *app_path);
 
 int main(int argc, char *argv[]) {
+  printf("Enter application\n");
   SDL_Init(0);
   font = new BDF_Font(font_fname);
 
   // setup display
+  printf("Setup display\n");
   int win_w = font->w * W;
   int win_h = font->h * H;
   screen = SDL_SetVideoMode(win_w, win_h, 32, SDL_HWSURFACE);
+
+  printf("Start Term\n");
 
   term = new Terminal(W, H);
 
