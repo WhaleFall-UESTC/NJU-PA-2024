73de9d39031db7fd83632a338da90e810b3b23ff
tracer-ics2023 <tracer@njuics.org>
2024-06-06 09:16:16 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:16:16 up 27 min,  2 users,  load average: 0.54, 0.39, 0.28

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -43,7 +43,7 @@ word_t trap_csr[4] = {};
 
 void set_trap_csr(int i, word_t value) { 
   if (i == satp) {
-    Log("set satp: %08x", value);
+    // Log("set satp: %08x", value);
     cpu.satp = value;
     return;
   }
@@ -51,7 +51,7 @@ void set_trap_csr(int i, word_t value) {
 }
 word_t get_trap_csr(int i) { 
   if (i == satp) {
-    Log("get satp: %08x", cpu.satp);
+    // Log("get satp: %08x", cpu.satp);
     return cpu.satp;
   }
   return trap_csr[i]; 
