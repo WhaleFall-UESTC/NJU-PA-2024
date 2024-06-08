52074585ea2dfac475d3a97f885979833e3441b3
tracer-ics2023 <tracer@njuics.org>
2024-06-09 07:21:39 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  07:21:39 up 47 min,  4 users,  load average: 0.61, 0.58, 0.89

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -11,7 +11,7 @@ extern void __am_get_cur_as(Context *c);
 extern void __am_switch(Context *c);
 
 Context* __am_irq_handle(Context *c) {
-  // __am_get_cur_as(c);
+  __am_get_cur_as(c);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
@@ -31,7 +31,7 @@ Context* __am_irq_handle(Context *c) {
     assert(c != NULL);
   }
 
-  // __am_switch(c);
+  __am_switch(c);
   return c;
 }
 
