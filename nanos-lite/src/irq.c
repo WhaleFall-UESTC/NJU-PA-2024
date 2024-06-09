5ffea66522b38cb8c67bd4e7e4d721881e55d728
tracer-ics2023 <tracer@njuics.org>
2024-06-09 21:46:13 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:46:13 up  1:24,  5 users,  load average: 2.26, 1.20, 0.81

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,7 +6,7 @@ Context* schedule(Context *prev);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case 1: Log("Detect EVENT_YIELD"); c = schedule(c); break;
-    case 2: /*Log("Detect EVENT_SYSCALL");*/ do_syscall(c); break;
+    case 2: Log("Detect EVENT_SYSCALL"); do_syscall(c); break;
     case 5: /*Log("Detect EVENT_IRQ_TIMER")*/; c = schedule(c); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
