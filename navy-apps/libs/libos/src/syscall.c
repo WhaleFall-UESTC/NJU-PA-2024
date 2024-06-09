695b3f1be94b740ec3a9bb49d512a9709b3e9c53
tracer-ics2023 <tracer@njuics.org>
2024-06-09 09:01:28 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:01:28 up  2:27,  5 users,  load average: 0.93, 1.18, 1.02

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -76,8 +76,8 @@ int _write(int fd, void *buf, size_t count) {
   return _syscall_(SYS_write, fd, (intptr_t)buf, count);
 }
 
-extern int _end;
-static intptr_t program_break = (intptr_t)&_end;
+extern int end;
+static intptr_t program_break = (intptr_t)&end;
 
 
 #define SYSTEM_CALL(...)                    \
@@ -90,7 +90,7 @@ static intptr_t program_break = (intptr_t)&_end;
 void *_sbrk(intptr_t increment) {
   // 下面这条代码永远钉在耻辱柱上
   // printf("sbrk, inc: %08x\n", increment);
-  if (_syscall_(SYS_brk, increment + program_break, 0, 0) == 0) {
+  if (_syscall_(SYS_brk, increment + program_break, increment, program_break) == 0) {
     intptr_t old = program_break;
     program_break += increment;
     return (void *)old;
