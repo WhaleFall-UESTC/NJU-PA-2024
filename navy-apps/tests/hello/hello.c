2823a360a44a8b66a95e322ee50a2b32984be91c
tracer-ics2023 <tracer@njuics.org>
2024-06-09 09:07:10 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:07:10 up  2:32,  5 users,  load average: 0.76, 0.78, 0.88

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -3,8 +3,9 @@
 #include <stdlib.h>
 
 int main() {
+  char *buf = (char *) malloc(100 * sizeof(char));
   write(1, "Hello World!\n", 13);
-  printf("Hello \n");
+  printf("Hello, %s\n", buf);
   write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
