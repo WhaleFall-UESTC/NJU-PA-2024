cfcc3bf5ac0e41c8c5eb6aa8516e5bc1df7f549b
tracer-ics2023 <tracer@njuics.org>
2024-06-09 15:37:12 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:37:12 up 9 min,  4 users,  load average: 0.57, 0.73, 0.41

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,227 +1,153 @@
+#include <sys/errno.h>
 #include <proc.h>
 #include <elf.h>
 #include <fs.h>
+#include <stdint.h>
 #include <memory.h>
-#include <am.h>
+
+#ifdef __LP64__
+# define Elf_Ehdr Elf64_Ehdr
+# define Elf_Phdr Elf64_Phdr
+# define Elf_Shdr Elf64_Shdr
+#else
+# define Elf_Ehdr Elf32_Ehdr
+# define Elf_Phdr Elf32_Phdr
+# define Elf_Shdr Elf32_Shdr
+#endif
 
 #if defined (__ISA_AM_NATIVE__)
-# define EXPECT_TYPE EM_X86_64
-#elif defined (__ISA_X86__)
-# define EXPECT_TYPE EM_386
-#elif defined (__ISA_MIPS32__)
-# define EXPECT_TYPE EM_MIPS
-#elif defined (__riscv)
-# define EXPECT_TYPE EM_RISCV
-#elif defined(__ISA_LOONGARCH32R__)
-# define EXPECT_TYPE EM_LOONGARCH32R
+# define EXPECT_VALUE EM_X86_64
+#elif defined(__ISA_X86__)
+# define EXPECT_VALUE EM_386
+#elif defined(__ISA_MIPS32__)
+# define EXPECT_VALUE EM_MIPS
+#elif defined(__riscv)
+# define EXPECT_VALUE EM_RISCV
 #else
-# error Unsupported ISA
+#error Unsupported ISA
 #endif
-
-#ifdef __LP64__
-#define Elf_Ehdr Elf64_Ehdr
-#define Elf_Phdr Elf64_Phdr
-#define Elf_Word Elf64_Word
-#define Elf_Half Elf64_Half
-#define Elf_Off Elf64_Off
-#define Elf_Addr Elf64_Addr
-#define Elf_Memz Elf64_Xword
-#define EhdrSize 0x34
-#else
-#define Elf_Ehdr Elf32_Ehdr
-#define Elf_Phdr Elf32_Phdr
-#define Elf_Word Elf32_Word
-#define Elf_Half Elf32_Half
-#define Elf_Off Elf32_Off
-#define Elf_Addr Elf32_Addr
-#define Elf_Memz Elf32_Word
-#define EhdrSize 0x28
-#endif
-
-#ifdef __LP64__
-#define Elf_Ehdr Elf64_Ehdr
-#define Elf_Phdr Elf64_Phdr
-#endif
-
-#define BUF 256
-
-// static void printEhdr(Elf_Ehdr ehdr);
-// static void printPhdr(Elf_Phdr phdr);
-
-static uintptr_t loader(PCB *pcb, const char *filename)
-{
-  Elf_Ehdr ehdr;
-  Elf_Phdr phdr;
-
-  #ifdef __LP64__
-  Log("Loading Elf64");
-  #endif
-
-  pcb->max_brk = 0;
-  int fd = fs_open(filename, 0, 0);
-  fs_read(fd, &ehdr, sizeof(Elf_Ehdr));
-  // printEhdr(ehdr);
-  // ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
-  assert(*((uint32_t *)(&ehdr.e_ident)) == 0x464c457f);
-  Elf_Addr entrypoint = ehdr.e_entry;
-
-  Elf_Off e_phoff = ehdr.e_phoff;
-  Elf_Half e_phentsize = ehdr.e_phentsize;
-  Elf_Half e_phnum = ehdr.e_phnum;
-
-  for (int i = 0; i < e_phnum; i++)
-  {
-    // ramdisk_read(&phdr, e_phoff + i * e_phentsize, e_phentsize);
-    fs_lseek(fd, e_phoff + i * e_phentsize, SEEK_SET);
-    fs_read(fd, &phdr, e_phentsize);
-
-    if ((Elf_Word)phdr.p_type != PT_LOAD)
-      continue;
-    
-
-    Elf_Memz memsz = phdr.p_memsz;
-    Elf_Memz nread = PGSIZE, pt;
-    Elf_Off phoff = phdr.p_offset;
-    void *pa = NULL;
-    void *va = (void *)phdr.p_vaddr;
-
-    // int port = (phdr.p_flags & PF_R ? MMAP_READ : 0)  |
-    //            (phdr.p_flags & PF_W ? MMAP_WRITE : 0) |  
-    //            (phdr.p_flags & PF_X ? MMAP_EXEC : 0);
-    int port = MMAP_READ | MMAP_EXEC | MMAP_WRITE;
-
-    for (pt = 0; pt <= memsz; pt += PGSIZE) {
-      if (memsz - pt < PGSIZE)
-        nread = memsz - pt;
-
-      pa = new_page(1);
-      map(&pcb->as, va, pa, port);
-      Log("New page at pa %p, mapped to va %p", pa, va);
-      fs_lseek(fd, phoff + pt, SEEK_SET);
-      fs_read(fd, pa, nread);
-      va += PGSIZE;
-    }
-
-    memset(((void *)(pa + nread)), 0, PGSIZE - nread);
-    if (phdr.p_vaddr + phdr.p_memsz > pcb->max_brk)
-      pcb->max_brk = phdr.p_vaddr + phdr.p_memsz;
-    pcb->max_brk = ROUNDUP(pcb->max_brk, PGSIZE);
-  }
-
-  Log("Loaded. Get entry: %08x", entrypoint);
-  return (uintptr_t) entrypoint;
-}
-
-void naive_uload(PCB *pcb, const char *filename)
-{
-  uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = %p", entry);
-  ((void (*)())entry)();
-}
 
 void context_kload(PCB *pcb, void (*entry)(void *), void *arg) {
-  pcb->cp = kcontext((Area) { pcb->stack, pcb + 1}, entry, arg);
+  pcb->cp = kcontext((Area){pcb, pcb + 1}, entry, arg);
 }
 
-void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]) {
-  Log("argv: %p, envp: %p", argv, envp);
-  // 为新进程创建上下文页表，并复制内核页表
-  protect(&pcb->as);
+int nanos_errno;
 
-  // 分页机制写入加载好的页表，获取入口点虚拟地址
+static uintptr_t loader(PCB *pcb, const char *filename) {
+  pcb->max_brk = 0;
+  Elf_Ehdr ehdr;
+  int fd = fs_open(filename, 0, 0);
+  if(fd < 0) {
+    nanos_errno = ENOENT;
+    Log("fs_open '%s' return with %d", filename, fd);
+    goto out;
+  }
+  fs_read(fd, &ehdr, sizeof ehdr);
+  if (memcmp(ehdr.e_ident, ELFMAG, SELFMAG) != 0
+      || ehdr.e_type != ET_EXEC
+      || ehdr.e_machine != EXPECT_VALUE
+      || ehdr.e_version != EV_CURRENT) {
+    nanos_errno = ENOEXEC;
+    goto out;
+  }
+  uint32_t nr_ph = ehdr.e_phnum;
+  if (nr_ph == PN_XNUM) {
+    Elf_Shdr shdr;
+    fs_lseek(fd, ehdr.e_shoff, SEEK_SET);
+    fs_read(fd, &shdr, sizeof shdr);
+    nr_ph = shdr.sh_info;
+  }
+  if (nr_ph * sizeof(Elf_Phdr) > PGSIZE) {
+    Log("Program header size too large (nr_ph = %d, file = %s)", nr_ph, filename);
+    nanos_errno = ENOEXEC;
+    goto out;
+  }
+  Elf_Phdr *phdr = (Elf_Phdr *)new_page(1);
+  fs_lseek(fd, ehdr.e_phoff, SEEK_SET);
+  fs_read(fd, phdr, nr_ph * sizeof(Elf_Phdr));
+  for (int i = 0; i < nr_ph; i++) {
+    if (phdr[i].p_type != PT_LOAD) continue;
+    if (phdr[i].p_memsz == 0) continue;
+    if (phdr[i].p_filesz > phdr[i].p_memsz) {
+      nanos_errno = ENOEXEC;
+      goto out_free;
+    }
+    if (phdr[i].p_align < 12) {
+      Log("Invalid alignment (p_align = %d, file = %s)", phdr[i].p_align, filename);
+      nanos_errno = ENOEXEC;
+      goto out_free;
+    }
+    uintptr_t va = phdr[i].p_vaddr;
+    uintptr_t va_hi = ROUNDDOWN(va, PGSIZE);
+    uintptr_t va_lo = va & (PGSIZE - 1);
+    uintptr_t va_end = va + phdr[i].p_memsz;
+    int nr_page = (ROUNDUP(va_end, PGSIZE) - ROUNDDOWN(va, PGSIZE)) / PGSIZE;
+    void *page = new_page(nr_page);
+    fs_lseek(fd, phdr[i].p_offset, SEEK_SET);
+    fs_read(fd, page + va_lo, phdr[i].p_filesz);
+    if (phdr[i].p_memsz > phdr[i].p_filesz) {
+      memset(page + va_lo + phdr[i].p_filesz, 0, phdr[i].p_memsz - phdr[i].p_filesz);
+    }
+    int prot = (phdr[i].p_flags & PF_R ? MMAP_READ : 0) |
+               (phdr[i].p_flags & PF_W ? MMAP_WRITE : 0);
+    for (int j = 0; j < nr_page; j++)
+      map(&pcb->as, (void *)va_hi + j * PGSIZE, page + j * PGSIZE, prot);
+    if (pcb->max_brk < va_end) pcb->max_brk = va_end;
+  }
+  pcb->max_brk = ROUNDUP(pcb->max_brk, PGSIZE);
+//   free_page(phdr);
+  return ehdr.e_entry;
+out_free:
+//   free_page(phdr);
+out:
+  return 0;
+}
+
+void naive_uload(PCB *pcb, const char *filename) {
+  uintptr_t entry = loader(pcb, filename);
+  Log("Jump to entry = %p", entry);
+  ((void(*)())entry) ();
+}
+
+#define NR_USTACKPG 8
+void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]) {
+  protect(&pcb->as);
   uintptr_t entry = loader(pcb, filename);
   if (!entry) {
     pcb->cp = NULL;
     return;
   }
-
-  // 参数入栈底
+  int argc, envc;
   void *page = new_page(NR_USTACKPG);
   void *sp = page + NR_USTACKPG * PGSIZE;
-
-  int argc, envc;
-  for (argc = 0; argv[argc] != NULL ; argc++);
-  for (envc = 0; envp[envc] != NULL ; envc++);
-  Log("Get argc: %d, envc: %d", argc, envc);
-  const int const_argc = argc, const_envc = envc;
-  char* argv_pt[const_argc];
-  char* envp_pt[const_envc];
-  for (int i = 0; i < argc; i++) {
-    Log("argv[%d]: %s", i, argv[i]);
-    sp -= strlen(argv[i]) + 1;
-    strcpy(sp, argv[i]);
-    argv_pt[i] = sp;
-  }
-
-  for (int i = 0; i < envc; i++) {
-    Log("envp[%d]: %s", i, envp[i]);
+  for (envc = 0; envp[envc]; envc++);
+  for (int i = envc - 1; i >= 0; i--) {
     sp -= strlen(envp[i]) + 1;
     strcpy(sp, envp[i]);
-    envp_pt[i] = sp;
   }
-
-  sp -= sizeof(void *);
-  *((uintptr_t *)sp) = 0;
-
-  for (int i = envc - 1; i >=0; i--) {
-    sp -= sizeof(char *);
-    *((uintptr_t *)sp) = (uintptr_t) envp_pt[i];
-  }
-
-  sp -= sizeof(void *);
-  *((uintptr_t *)sp) = 0;
-
+  for (argc = 0; argv[argc]; argc++);
   for (int i = argc - 1; i >= 0; i--) {
-    sp -= sizeof(char *);
-    *((uintptr_t *)sp) = (uintptr_t) argv_pt[i];
+    sp -= strlen(argv[i]) + 1;
+    strcpy(sp, argv[i]);
   }
-
-  sp -= sizeof(void *);
-  *((uintptr_t *)sp) = argc;
-
-  free(argv_pt);
-  free(envp_pt);
-
-
-  // 栈空间实现分页，写入到内核栈的上下文
+  uintptr_t *p = (uintptr_t *)ROUNDDOWN(
+      sp - sizeof(void *) * (1 + argc + 1 + envc + 1), sizeof(void *));
+  p[0] = (uintptr_t)argc;
+  for (int i = 0; i < argc; i++) {
+    p[i + 1] = (uintptr_t)sp;
+    sp += strlen(argv[i]) + 1;
+  }
+  p[argc + 1] = 0;
+  for (int i = 0; i < envc; i++) {
+    p[argc + 2 + i] = (uintptr_t)sp;
+    sp += strlen(envp[i]) + 1;
+  }
+  p[argc + 2 + envc] = 0;
   void *vpage = pcb->as.area.end - NR_USTACKPG * PGSIZE;
   for (int i = 0; i < NR_USTACKPG; i++) {
-    map(&pcb->as, vpage + PGSIZE * i, page + PGSIZE * i, MMAP_READ | MMAP_WRITE);
+    map(&pcb->as, vpage + PGSIZE * i,
+        page + PGSIZE * i, MMAP_WRITE);
   }
-
-  // 为内核栈（pcb[0],[1]...）加载上下文（本来pcb就是这些）
-  // 包括程序入口点，sp，mepc，pdir（来自as）
-  pcb->cp = ucontext(&pcb->as, (Area) { pcb, pcb + 1 }, (void *)entry);
-  Log("sp = %08x\t", (uintptr_t) sp + vpage - page);
-  // 将 pcb 上下文的返回值 a0 设为当前栈顶的虚拟地址
-  pcb->cp->GPRx = (uintptr_t) sp + vpage - page;
+  pcb->cp = ucontext(&pcb->as, (Area){pcb, pcb + 1}, (void *)entry);
+  pcb->cp->GPRx = vpage + (uintptr_t)p - page;
 }
-
-
-// static void printEhdr(Elf_Ehdr ehdr) {
-//   printf("magic = %#08x\n", *((uint32_t *)(&ehdr.e_ident)));
-//   printf("e_type = %#08x\n", ehdr.e_type);
-//   printf("e_machine = %#08x\n", ehdr.e_machine);
-//   printf("e_version = %#08x\n", ehdr.e_version);
-//   printf("e_entry = %#08x\n", ehdr.e_entry);
-//   printf("e_phoff = %#08x\n", ehdr.e_phoff);
-//   printf("e_shoff = %#08x\n", ehdr.e_shoff);
-//   printf("e_flags = %#08x\n", ehdr.e_flags);
-//   printf("e_ehsize = %d\n", ehdr.e_ehsize);
-//   printf("e_phentsize = %d\n", ehdr.e_phentsize);
-//   printf("e_phnum = %d\n", ehdr.e_phnum);
-//   printf("e_shentsize = %d\n", ehdr.e_shentsize);
-//   printf("e_shnum = %d\n", ehdr.e_shnum);
-//   printf("e_shstrndx = %#08x\n", ehdr.e_shstrndx);
-// }
-
-// static void printPhdr(Elf_Phdr phdr) {
-//   printf("p_type = %#08x\n", phdr.p_type);
-//   printf("p_offset = %#08x\n", phdr.p_offset);
-//   printf("p_vaddr = %#08x\n", phdr.p_vaddr);
-//   printf("p_paddr = %#08x\n", phdr.p_paddr);
-//   printf("p_filesz = %#08x\n", phdr.p_filesz);
-//   printf("p_memsz = %#08x\n", phdr.p_memsz);
-//   printf("p_flags = %#08x\n", phdr.p_flags);
-//   printf("p_align = %#08x\n", phdr.p_align);
-// }
