28e17292717b9301c397d29c347981041991f996
tracer-ics2023 <tracer@njuics.org>
2024-06-09 23:38:08 +0800

    >  compile NEMU 2023080902011 WhaleFall Linux thx 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:38:08 up  3:16,  8 users,  load average: 0.61, 0.58, 0.70

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -19,7 +19,7 @@ static int screen_H = 0, screen_W = 0;
 static bool gpu_cfg = 0;
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  yield();
+  // yield();
   for (size_t i = 0; i < len; i++) {
     putch(((char *)buf)[i]);
   }
@@ -27,7 +27,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  yield();
+  // yield();
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if (ev.keycode == AM_KEY_NONE) {
     return 0;
@@ -41,7 +41,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  yield();
+  // yield();
   assert((offset & 3) == 0 && (len & 3) == 0);
   int x = offset / 4 % screen_W;
   int y = offset / 4 / screen_W;
